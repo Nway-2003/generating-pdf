e7976522bd9ec096804463aff5e631f043e4290a
Nway-2003 <smps-ls05633@learning.educlaas.com>
2024-08-07 17:12:45 +0630

    your comment

---
+++ generating-pdf/src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <v-app>
+    <v-container>
+      <PrintButton />
+    </v-container>
+  </v-app>
+</template>
+
+<script setup>
+import PrintButton from './components/Pdf.vue'
+</script>
+
+<style scoped>
+/* Add your styles here */
+</style>
